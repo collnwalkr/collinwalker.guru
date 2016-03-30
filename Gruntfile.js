b6be0d5b0cc80b6550bc1609255e253e27349be4
collnwalkr <collinwalkerdesigns@gmail.com>
2016-03-30 11:10:06 -0700

    grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        //LINT javascript files
+        jshint: {
+            files: ['Gruntfile.js', 'js/*.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+
+
+        //MINIFY javascript files
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'js/weekli.min.js': ['js/weekli.js']
+                }
+            }
+        },
+
+
+        //COMPILE less
+        less: {
+            // production config is also available
+            development: {
+                options: {
+                    paths: ['less/']
+                },
+                files: {
+                    //compilation.css  :  source.less
+                    "css/weekli.css": "css/weekli.less",
+                    "css/index.css": "css/index.less"
+                }
+            }
+        },
+
+        //CONNECT and SERVE on browser
+        connect: {
+            server: {
+                options: {
+                    port: 9002
+                }
+            }
+        },
+
+        watch: {
+            files: ['js/*.js', 'js/*.js', 'css/*.less'],
+            tasks: ['jshint', 'uglify', 'less']
+        },
+
+        bump: {
+            options: {
+                files: ['package.json'],
+                commit: false,
+                push: false
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-bump');
+
+    grunt.registerTask('default', ['jshint', 'uglify', 'less','connect', 'watch']);
+};
