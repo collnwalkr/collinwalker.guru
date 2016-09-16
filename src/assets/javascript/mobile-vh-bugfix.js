250e152436ec17c4c56e93a8ce5ef9e854f9ca31
collnwalkr <collinwalkerdesigns@gmail.com>
2016-09-16 12:48:47 -0700

    temporarily removed vh-bugfix and updated some styling

--- src/assets/javascript/mobile-vh-bugfix.js
+++ src/assets/javascript/mobile-vh-bugfix.js
@@ -2,11 +2,17 @@
  Based off fix from Dave Molinero: http://www.davemolinero.com/2015/09/21/quick-fix-for-the-100vh-mobile-adress-bar-bug/
  */
 $( document ).ready(function() {
-  if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    var h = $('.full-height').height();
-    $('.full-height').height(h);
 
-    var r = $('.half-height').height();
-    $('.half-height').height(r);
-  }
+  //if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  //}
+
 });
+
+function set_height(){
+
+  var h = $(window).height();
+  $('.full-height').height(h);
+
+  var r = $(window).height() / 2;
+  $('.half-height').height(r);
+}
