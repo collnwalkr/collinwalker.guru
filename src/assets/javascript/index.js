b6238e6d0ba049a5078da6a2c95302eafc468952
collnwalkr <collinwalkerdesigns@gmail.com>
2016-10-12 16:54:18 -0700

    transition between pages done

--- src/assets/javascript/index.js
+++ src/assets/javascript/index.js
@@ -5,6 +5,13 @@
 
 $( document ).ready(function() {
 
+
+  $(window).bind("pageshow", function(event) {
+    if (event.originalEvent.persisted) {
+      window.location.reload()
+    }
+  });
+
   $(function () {
     if ($('article').hasClass('index')) {
 
@@ -50,9 +57,52 @@ $( document ).ready(function() {
         }
       );
 
+
+
+      // animate screen transitions
+      $(function(){
+
+        // get all of the links
+        var a_links = $('.project-contents').find(' a ');
+
+        // give them handlers
+        for( var i = 0; i < a_links.length; i++){
+          (function( index ){
+            a_links[ i ].addEventListener( 'click', function(e){
+
+              // prevent default
+              e.preventDefault();
+
+              // if the link is valid
+              if(a_links[index].href){
+
+                // add the overlay
+                var zoom_overlay = '<div class=\'zoom-overlay\'></div>';
+                $(zoom_overlay).appendTo(document.body);
+
+                // fade in overlay
+                setTimeout(function(){
+                  $(document.body).addClass('zoom-overlay-open transition-overlay');
+                }, 100);
+
+                // animate to top of screen
+                $("html, body").animate({ scrollTop: $('body').offset().top }, 100);
+
+                // change to next page
+                setTimeout(function(){
+                  window.location.href = a_links[index].href;
+                }, 100);
+              }
+            });
+          })( i );
+
+        }
+      });
+
     }
 
   });
+
 
 
   function show_image(img) {
