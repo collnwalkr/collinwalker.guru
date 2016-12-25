80658d8f24fe0d0794c5654c01526f0b82a806b2
collnwalkr <collinwalkerdesigns@gmail.com>
2016-12-25 08:51:04 -0800

    Made canvas retina. (need to return to this to make it better detect high density displays)

--- src/assets/javascript/index-canvas.js
+++ src/assets/javascript/index-canvas.js
@@ -14,17 +14,31 @@ $( document ).ready(function() {
       //canvas dimensions
       var W = window.innerWidth;
       var H = window.innerHeight;
+      if(W < 500){
+        W *= 2;
+        H *= 2;
+        ctx.scale(2,2);
+      } else{
+        ctx.scale(1,1);
+      }
       canvas.width = W;
       canvas.height = H;
+
 
       $(window).resize(function(){
         W = window.innerWidth;
         H = window.innerHeight;
+        if(W < 500){
+          W *= 2;
+          H *= 2;
+          ctx.scale(2,2);
+        } else{
+          ctx.scale(1,1);
+        }
         canvas.width = W;
         canvas.height = H;
       });
 
-      //snowflake particles
       var mp = 10; //max particles
       var particles = [];
       for(var i = 0; i < mp; i++)
@@ -38,12 +52,11 @@ $( document ).ready(function() {
         })
       }
 
-      //Lets draw the flakes
       function draw()
       {
         ctx.clearRect(0, 0, W, H);
 
-        ctx.fillStyle = "rgba(202, 106, 255, 0.5)";
+        ctx.fillStyle = "rgba(202, 106, 255, 0.4)";
         for(var i = 0; i < mp; i++)
         {
           var p = particles[i];
@@ -59,8 +72,6 @@ $( document ).ready(function() {
         update();
       }
 
-      //Function to move the snowflakes
-      //angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
       var angle = 0;
       function update()
       {
@@ -68,36 +79,17 @@ $( document ).ready(function() {
         for(var i = 0; i < mp; i++)
         {
           var p = particles[i];
-          p.y += (Math.cos(angle+p.d) + 1 + p.r) / 4;
+          p.y += (Math.cos(angle * p.r) + p.r) / 4;
           p.x += Math.sin(angle);
           p.o = p.x * 4;
-          //isNaN(p.o) ? p.o = 0 : p.o += 1;
 
-          if(p.x > W+5 || p.x < -5 || p.y > H)
+          if(p.y > H)
           {
-            if(i%3 > 0) //66.67% of the flakes
-            {
-              particles[i] = {x: Math.random()*W, y: -10, r: Math.random()+1 + W/1000, d: p.d, o: p.o};
-            }
-            else
-            {
-              //If the flake is exitting from the right
-              if(Math.sin(angle) > 0)
-              {
-                //Enter from the left
-                particles[i] = {x: -5, y: H, r: p.r, d: p.d, o: p.o};
-              }
-              else
-              {
-                //Enter from the right
-                particles[i] = {x: W+5, y: H, r: p.r, d: p.d, o: p.o};
-              }
-            }
+            particles[i] = {x: Math.random()*W, y: -10, r: Math.random()+1 + W/1000, d: p.d, o: p.o};
           }
         }
       }
 
-      //animation loop
       setInterval(draw, 5);
     }
   })
